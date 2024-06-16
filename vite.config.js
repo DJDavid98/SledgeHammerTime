fb63ac91ec0521289d3cc07698a0bbd74b9a34e7
DJDavid98 <djdavid98@protonmail.com>
2024-06-16 17:17:43 +0200

    Exclude vite laravel plugin only during test

--- vite.config.js
+++ vite.config.js
@@ -4,39 +4,41 @@ import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
 import i18n from 'laravel-vue-i18n/vite';
 
-const isCI = process.env.CI === 'true';
+export default defineConfig(({ mode }) => {
+  const isTest = process.env.NODE_ENV === 'test' || mode === 'test';
 
-const conditionalPlugins = [];
-if (!isCI) {
-  conditionalPlugins.push(laravel({
-    input: 'resources/js/app.ts',
-    ssr: 'resources/js/ssr.ts',
-    refresh: true,
-  }));
-}
+  const conditionalPlugins = [];
+  if (!isTest) {
+    conditionalPlugins.push(laravel({
+      input: 'resources/js/app.ts',
+      ssr: 'resources/js/ssr.ts',
+      refresh: true,
+    }));
+  }
 
-export default defineConfig({
-  resolve: {
-    alias: {
-      '~@picocss': resolve(__dirname, 'node_modules/@picocss'),
-      '@': resolve(__dirname, 'resources/js'),
-    },
-  },
-  plugins: [
-    ...conditionalPlugins,
-    vue({
-      template: {
-        transformAssetUrls: {
-          base: null,
-          includeAbsolute: false,
-        },
+  return ({
+    resolve: {
+      alias: {
+        '~@picocss': resolve(__dirname, 'node_modules/@picocss'),
+        '@': resolve(__dirname, 'resources/js'),
       },
-    }),
-    i18n(),
-  ],
-  test: {
-    sequence: {
-      shuffle: true,
     },
-  },
+    plugins: [
+      ...conditionalPlugins,
+      vue({
+        template: {
+          transformAssetUrls: {
+            base: null,
+            includeAbsolute: false,
+          },
+        },
+      }),
+      i18n(),
+    ],
+    test: {
+      sequence: {
+        shuffle: true,
+      },
+    },
+  });
 });
