3e7e4f8d2e0f93ff2f1c7f3f1cb96594961f8d6b
Double Colon <djdavid98@protonmail.com>
2024-12-30 04:06:03 +0100

    New translations global.php (Hungarian)

--- lang/hu/global.php
+++ lang/hu/global.php
@@ -1,20 +1,11 @@
 <?php
 
 return [
-  'seoDescription' => 'Generate timestamp indicators for Discord chat messages',
-  'changeLanguage' => 'Change language',
-  'guest' => 'Guest',
+  'seoDescription' => 'Generálj időpont jelölőket a Discord üzeneteidhez',
+  'changeLanguage' => 'Nyelv változtatás',
+  'guest' => 'Látogató',
   'nav' => [
-    'botSettings' => 'Bot Settings',
-    'profile' => 'Profile',
-    'login' => 'Log in',
-    'logout' => 'Log out',
-  ],
-  'form' => [
-    'save' => 'Save',
-    'saved' => 'Saved.',
-    'cancel' => 'Cancel',
-    'select' => 'Select',
-    'confirm' => 'Confirm',
+    'botSettings' => 'Bot Beállítások',
+    'profile' => 'Profil',
   ],
 ];
