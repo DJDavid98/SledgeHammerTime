b981bf10947f32a5632b9e6958a93b6704a727be
Double Colon <djdavid98@protonmail.com>
2024-12-30 03:29:14 +0100

    New translations botsettings.php (Hungarian)

--- lang/hu/botSettings.php
+++ lang/hu/botSettings.php
@@ -1,7 +1,42 @@
 <?php
 
 return [
-  'title' => 'Bot Beállítások',
-  'description' => 'Alább láthatod a jelenlegi beállításaidat a HammerTime Bot-ban, minden összekapcsolt fiókodhoz.',
-
+  'title' => 'Bot Settings',
+  'description' => 'Below you can see your current settings in the HammerTime Bot for each connected account.',
+  'fields' => [
+    'rawData' => [
+      'displayName' => 'Raw data (for debugging purposes)',
+    ],
+    'timezone' => [
+      'displayName' => 'Timezone',
+    ],
+    'format' => [
+      'displayName' => 'Format',
+      'option' => [
+        'd' => 'Short date',
+        'f' => 'Date and time',
+        't' => 'Short time',
+        'D' => 'Long date',
+        'F' => 'Weekday, date and time',
+        'R' => 'Relative',
+        'T' => 'Long time',
+        'default' => 'Default',
+      ],
+    ],
+    'columns' => [
+      'displayName' => 'Columns',
+      'option' => [
+        'syntax' => 'Syntax only',
+        'preview' => 'Preview only',
+        'both' => 'Both syntax and preview',
+        'default' => 'Default',
+      ],
+    ],
+    'ephemeral' => [
+      'displayName' => 'Ephemeral',
+    ],
+    'header' => [
+      'displayName' => 'Header',
+    ],
+  ],
 ];
