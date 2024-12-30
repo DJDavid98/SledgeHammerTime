1edf1f65d1907d464f75b11d8151c5157965cea4
Double Colon <djdavid98@protonmail.com>
2024-12-30 03:30:15 +0100

    New translations global.php (French)

---
+++ lang/fr/global.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+  'seoDescription' => 'Generate timestamp indicators for Discord chat messages',
+  'changeLanguage' => 'Change language',
+  'guest' => 'Guest',
+  'nav' => [
+    'botSettings' => 'Bot Settings',
+    'profile' => 'Profile',
+    'login' => 'Log in',
+    'logout' => 'Log out',
+  ],
+  'form' => [
+    'save' => 'Save',
+    'saved' => 'Saved.',
+    'cancel' => 'Cancel',
+    'select' => 'Select',
+    'confirm' => 'Confirm',
+  ],
+];
