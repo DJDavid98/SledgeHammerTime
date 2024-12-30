17d9a7facf074fa4ef8941df8b4d3b64538f7ec0
Double Colon <djdavid98@protonmail.com>
2024-12-30 04:06:26 +0100

    New translations global.php (Malay)

--- lang/ms/global.php
+++ lang/ms/global.php
@@ -1,20 +1,11 @@
 <?php
 
 return [
-  'seoDescription' => 'Generate timestamp indicators for Discord chat messages',
+  'seoDescription' => 'Jana penunjuk cap masa untuk mesej sembang Discord',
   'changeLanguage' => 'Change language',
   'guest' => 'Guest',
   'nav' => [
     'botSettings' => 'Bot Settings',
     'profile' => 'Profile',
-    'login' => 'Log in',
-    'logout' => 'Log out',
-  ],
-  'form' => [
-    'save' => 'Save',
-    'saved' => 'Saved.',
-    'cancel' => 'Cancel',
-    'select' => 'Select',
-    'confirm' => 'Confirm',
   ],
 ];
