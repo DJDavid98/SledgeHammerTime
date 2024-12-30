4c1c80aea1b93a0747ed6c8247255f1fc4162fcb
Double Colon <djdavid98@protonmail.com>
2024-12-30 04:05:54 +0100

    New translations global.php (Arabic)

--- lang/ar/global.php
+++ lang/ar/global.php
@@ -1,20 +1,11 @@
 <?php
 
 return [
-  'seoDescription' => 'Generate timestamp indicators for Discord chat messages',
-  'changeLanguage' => 'Change language',
+  'seoDescription' => 'قم بعمل مؤشرات ختم زمني لرسائل ديسكورد',
+  'changeLanguage' => 'تغيير اللغة',
   'guest' => 'Guest',
   'nav' => [
     'botSettings' => 'Bot Settings',
     'profile' => 'Profile',
-    'login' => 'Log in',
-    'logout' => 'Log out',
-  ],
-  'form' => [
-    'save' => 'Save',
-    'saved' => 'Saved.',
-    'cancel' => 'Cancel',
-    'select' => 'Select',
-    'confirm' => 'Confirm',
   ],
 ];
