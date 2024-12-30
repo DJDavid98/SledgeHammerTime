8516e1475bd3b7c40640f96206e67e3231e02ea7
Double Colon <djdavid98@protonmail.com>
2024-12-30 04:04:58 +0100

    New translations auth.php (Malay)

--- lang/ms/auth.php
+++ lang/ms/auth.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 return [
-    'failed'   => 'These credentials do not match our records.',
-    'password' => 'The password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed'   => 'Butiran ini tidak sepadan dengan rekod kami.',
+    'password' => 'Kata laluan tidak sah.',
+    'throttle' => 'Terlalu banyak percubaan log masuk. Sila cuba lagi dalam :seconds saat.',
 ];
