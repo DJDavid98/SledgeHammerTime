38672ccc55469e4950194a4f73684ae5fbccce60
Double Colon <djdavid98@protonmail.com>
2025-02-02 00:16:02 +0100

    New translations addapp.php (Danish)

---
+++ lang/da/addApp.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+  "title" => "Select where you would like to add the app",
+  "user" => [
+    "title" => "Add to My Apps",
+    "description" => "Use this app everywhere!",
+  ],
+  "guild" => [
+    "title" => "Add to Server",
+    "description" => "Customize your server by adding this app",
+  ],
+];
