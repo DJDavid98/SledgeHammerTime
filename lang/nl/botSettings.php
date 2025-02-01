50cf88fc28841a80ab59e789b887de8e2ccc60e9
Double Colon <djdavid98@protonmail.com>
2025-02-02 00:25:26 +0100

    New translations botsettings.php (Dutch)

--- lang/nl/botSettings.php
+++ lang/nl/botSettings.php
@@ -1,43 +1,43 @@
 <?php
 
 return [
-  'title' => 'Bot-instellingen',
-  'description' => 'Hieronder ziet u uw huidige instellingen in de HammerTime Bot voor elke verbonden account.',
+  'title' => 'Bot Settings',
+  'description' => 'Below you can see your current settings in the HammerTime Bot for each connected account.',
   'fields' => [
     'rawData' => [
-      'displayName' => 'Ruwe gegevens (voor debugging-doeleinden)',
+      'displayName' => 'Raw data (for debugging purposes)',
     ],
     'timezone' => [
-      'displayName' => 'Tijdzone',
+      'displayName' => 'Timezone',
     ],
     'format' => [
       'displayName' => 'Format',
       'option' => [
-        'd' => 'Korte datum',
-        'f' => 'Datum en tijd',
-        't' => 'Korte tijd',
-        'D' => 'Lange datum',
-        'F' => 'Weekdag, datum en tijd',
-        'R' => 'Relatief',
-        'T' => 'Lange tijd',
-        'default' => 'Standaard',
+        'd' => 'Short date',
+        'f' => 'Date and time',
+        't' => 'Short time',
+        'D' => 'Long date',
+        'F' => 'Weekday, date and time',
+        'R' => 'Relative',
+        'T' => 'Long time',
+        'default' => 'Default',
       ],
     ],
     'columns' => [
-      'displayName' => 'Kolommen',
+      'displayName' => 'Columns',
       'option' => [
         'syntax' => 'Syntax only',
         'preview' => 'Preview only',
         'both' => 'Both syntax and preview',
-        'default' => 'Standaard',
+        'default' => 'Default',
       ],
     ],
     'ephemeral' => [
       'displayName' => 'Ephemeral',
     ],
     'header' => [
-      'displayName' => 'Koptekst',
+      'displayName' => 'Header',
     ],
   ],
-  'saveSuccess' => 'Uw instellingen zijn succesvol opgeslagen.',
+  'saveSuccess' => 'Your settings have been saved successfully.',
 ];
