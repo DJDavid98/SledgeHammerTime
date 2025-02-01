ac3f02e537435b625850a7eb7da5157cb43e6e42
Double Colon <djdavid98@protonmail.com>
2025-02-01 18:31:34 +0100

    New translations global.php (Dutch)

--- lang/nl/global.php
+++ lang/nl/global.php
@@ -8,4 +8,31 @@ return [
     'botSettings' => 'Bot-instellingen',
     'profile' => 'Profiel',
   ],
+  'sidebar' => [
+    'inputSettings' => [
+      'title' => 'Input settings',
+      'customInputs' => [
+        'label' => 'Custom inputs',
+        'description' => "Replaces the default inputs with ones that match the current language instead of your browser's",
+      ],
+      'separateInputs' => [
+        'label' => 'Separate inputs',
+        'description' => "Display two different inputs for date and time instead of a combined one (which is not supported in some browsers)",
+      ],
+    ],
+    'credits' => [
+      'title' => 'Credits',
+      'developedBy' => 'Developed by <1></1>',
+      'using' => 'Using <1></1>',
+      'fontAwesomeFree' => 'Font Awesome Free',
+      'laravel' => 'Laravel',
+      'vueJs' => 'Vue.js',
+      'translatedBy' => 'Translations by <1></1>',
+      'openSourceSoftware' => 'Open-source software',
+      'viewSourceCode' => 'View source code',
+      'notAffiliated' => 'This project is not affiliated with Discord.',
+      'incompleteTranslations' => 'Translations are incomplete',
+      'contributeTranslations' => 'Contribute',
+    ],
+  ],
 ];
