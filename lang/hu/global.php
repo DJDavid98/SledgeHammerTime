2a52c700995a86acd4cb10fd7c1b977a36d152ed
Double Colon <djdavid98@protonmail.com>
2025-02-02 20:39:20 +0100

    New translations global.php (Hungarian)

--- lang/hu/global.php
+++ lang/hu/global.php
@@ -6,6 +6,10 @@ return [
   'incompleteTranslations' => 'A fordítások hiányosak',
   'contributeTranslations' => 'Közreműködés',
   'guest' => 'Látogató',
+  'jsDisabled' => [
+    'title' => 'JavaScript is necessary',
+    'body' => "Your browser either does not support JavaScript or it's currently disabled. Some browsers disable JavaScript by default for security reasons, but it's necessary for this application. Please enable it and refresh the page, or use a different browser.",
+  ],
   'nav' => [
     'botSettings' => 'Bot Beállítások',
     'profile' => 'Profil',
