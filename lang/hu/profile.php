9ae594c05ceb566700f55325922734c662c6511f
Double Colon <djdavid98@protonmail.com>
2025-04-28 21:33:01 +0200

    New translations profile.php (Hungarian)

--- lang/hu/profile.php
+++ lang/hu/profile.php
@@ -14,6 +14,12 @@ return [
       'A HammerTime account is going to let you link multiple Discord accounts to be able to manage their resources in one place in the future.',
       'Below you can find a list of all Discord accounts associated with this HammerTime account.',
     ],
+    'noConnectedAccounts' => 'You do not have any accounts connected at the moment',
+    'linkAdditional' => [
+      'heading' => 'Link or Update Account',
+      'description' => 'By linking multiple Discord accounts to a single HammerTime account, you can manage all connected accounts\' settings in a single place. If your profile picture or display name changed, you can also use this to update that information for any account which is already linked.',
+      'authorize' => 'Authorize Discord account',
+    ],
   ],
   'deletion' => [
     'heading' => 'Account Deletion',
