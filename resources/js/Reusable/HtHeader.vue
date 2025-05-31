2bec505d61f7b4c6c4f4feba6a71cc68c67797cb
WentTheFox <mail@went.tf>
2025-05-31 21:50:47 +0200

    Remove header stickyness on legal page

--- resources/js/Reusable/HtHeader.vue
+++ resources/js/Reusable/HtHeader.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
 import ApplicationLogo from '@/Components/ApplicationLogo.vue';
 import { getAppName } from '@/utils/app';
-import { Link } from '@inertiajs/vue3';
+import { Link, usePage } from '@inertiajs/vue3';
+import { computed } from 'vue';
 
 const appName = getAppName();
+const page = usePage();
+const nonStick = computed(() => page.props.ziggy.location === route('legal', route().params));
 </script>
 
 <template>
-  <div class="header">
+  <div :class="['header', {'non-stick': nonStick}]">
     <div class="header-left">
       <slot name="left" />
     </div>
